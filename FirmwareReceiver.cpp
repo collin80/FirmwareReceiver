9aaba791d3af1a20d4a0fcd5c183ca1f80061679
Collin Kidder <collink@kkmfg.com>
2015-09-01 21:30:15 -0400

    Additional debugging, renamed a variable.

--- FirmwareReceiver.cpp
+++ FirmwareReceiver.cpp
@@ -1,6 +1,6 @@
 #include "FirmwareReceiver.h"
 
-CAN_FRAME out;
+CAN_FRAME outFrame;
 
 FirmwareReceiver::FirmwareReceiver(CANRaw *bus, uint32_t token, uint32_t base)
 {
@@ -19,7 +19,9 @@ FirmwareReceiver::FirmwareReceiver(CANRaw *bus, uint32_t token, uint32_t base)
 
 void FirmwareReceiver::gotFrame(CAN_FRAME *frame)
 {	
-	int location, bufferWritePtr;					
+	int location, bufferWritePtr;	
+
+	//SerialUSB.println("Got frame");
 
 	if (frame->id == baseAddress)
 	{
@@ -34,18 +36,18 @@ void FirmwareReceiver::gotFrame(CAN_FRAME *frame)
 						SerialUSB.println("Starting firmware upload process");
 						flashWritePosition = 0;
 						updatingFirmware = true;		
-						out.id = baseAddress + 0x10;
-						out.extended = false;
-						out.length = 8;
-						out.data.byte[0] = 0xDE;
-						out.data.byte[1] = 0xAF;
-						out.data.byte[2] = 0xDE;
-						out.data.byte[3] = 0xED;
-						out.data.byte[4] = (deviceToken & 0xFF);
-						out.data.byte[5] = ((deviceToken >> 8) & 0xFF);
-						out.data.byte[6] = ((deviceToken >> 16) & 0xFF);
-						out.data.byte[7] = ((deviceToken >> 24) & 0xFF);
-						canbus->sendFrame(out);
+						outFrame.id = baseAddress + 0x10;
+						outFrame.extended = false;
+						outFrame.length = 8;
+						outFrame.data.byte[0] = 0xDE;
+						outFrame.data.byte[1] = 0xAF;
+						outFrame.data.byte[2] = 0xDE;
+						outFrame.data.byte[3] = 0xED;
+						outFrame.data.byte[4] = (deviceToken & 0xFF);
+						outFrame.data.byte[5] = ((deviceToken >> 8) & 0xFF);
+						outFrame.data.byte[6] = ((deviceToken >> 16) & 0xFF);
+						outFrame.data.byte[7] = ((deviceToken >> 24) & 0xFF);
+						canbus->sendFrame(outFrame);
 					}
 				}
 			}
@@ -71,12 +73,12 @@ void FirmwareReceiver::gotFrame(CAN_FRAME *frame)
 			dueFlashStorage.write(flashWritePosition, pageBuffer, IFLASH1_PAGE_SIZE, flashSection);
 			flashWritePosition += IFLASH1_PAGE_SIZE;
 		}		
-		out.id = baseAddress + 0x20;
-		out.extended = false;
-		out.length = 2;
-		out.data.byte[0] = frame->data.byte[0];
-		out.data.byte[1] = frame->data.byte[1];
-		canbus->sendFrame(out);
+		outFrame.id = baseAddress + 0x20;
+		outFrame.extended = false;
+		outFrame.length = 2;
+		outFrame.data.byte[0] = frame->data.byte[0];
+		outFrame.data.byte[1] = frame->data.byte[1];
+		canbus->sendFrame(outFrame);
 	}
 
 	if (frame->id == (baseAddress + 0x30))
@@ -86,12 +88,13 @@ void FirmwareReceiver::gotFrame(CAN_FRAME *frame)
 			if ((frame->data.byte[2] == 0xFA) && (frame->data.byte[3] == 0xDE))
 			{
 				//write out the any last little bits that were received but didn't add up to a full page
+				SerialUSB.println(bufferWritePtr);
 				SerialUSB.print("Writing flash at section ");
 				SerialUSB.print(flashSection);
 				SerialUSB.print("     address ");
 				SerialUSB.println(flashWritePosition);
-				dueFlashStorage.write(flashWritePosition, pageBuffer, bufferWritePtr, flashSection);
-
+				dueFlashStorage.write(flashWritePosition, pageBuffer, bufferWritePtr + 3, flashSection);
+				SerialUSB.println("About to set boot mode");
 				//switch boot section
 				if (flashSection = 0) dueFlashStorage.setGPNVMBootMode(false);
 				else dueFlashStorage.setGPNVMBootMode(true);
